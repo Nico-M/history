19d8dffe12d744b8f67b26e7996bce27c1cc4874
nico <531521711@qq.com>
2016-11-20 15:43:39 +0800

    更新

---
+++ html/js/search2.js
@@ -0,0 +1,151 @@
+$('input:submit').on('click', function () {
+    $('.list ul').html('')
+    var keyword = $('input:text').val();
+    //请求豆瓣数据
+    $.ajax({
+        type: 'get',
+        url: 'https://www.xiami.com/search/json',
+        data: {
+            k: keyword,
+            t: 1
+        },
+
+        dataType: "jsonp",
+        jsonCallback: '?',
+        jsonp: 'callback',
+        //success:function(data){
+        //    console.log(data)
+        //}
+        success: function (data) {
+            //var mydata = data.musics;
+            //console.log(data);
+            //注意要先设置,叠加
+            var str = '';
+            var imgUrl = 'http://img.xiami.net/'
+            $.each(data, function (index, value) {
+                str += '<li>\
+                <dl class="clearfix">\
+                    <dt class="images"><img src=' + imgUrl + this.album_logo + ' alt=""></dt>\
+                    <dd class="">\
+                        <div class="title">曲名:<span>' + this.song_name + '</span> </div>\
+                        <div class="link">\
+                            歌手: <span>' + this.artist_name + '</span>\
+                        </div>\
+                        <div class="song">\
+                            <a href="#" class="jump">播放</a>\
+                            <button type="" id=' + this.song_id + ' ></button>\
+                        </div>\
+                    </dd>\
+                </dl>\
+            </li>';
+                // console.log(str)
+
+
+            });
+            // console.log(str)
+            // 加载到界面
+            $('.list ul').append($(str));
+            //var onOff = true;
+            var audio = $('#audio')[0];
+
+            function startPlay(dom) {
+                audio.load();
+                audio.play();
+                $('.song button').css('backgroundImage', 'url(images/play.png)');
+                dom.css('backgroundImage', 'url(images/pause.png)');
+                $('.icon li').first().css('backgroundPosition','-492px 0px');
+            }
+
+            function stopPlay(dom) {
+                audio.pause();
+                dom.css('backgroundImage', 'url(images/play.png)')
+                $('.icon li').first().css('backgroundPosition','-324px 0px');
+            }
+            $('.song button').attr('onoff',true);
+            $('.song button').click(function () {
+                //this.onoff = onOff;
+                var song_id = this.id;
+                console.log(song_id)
+                    //console.log($('button').length);
+                var $self = $(this)
+                var self = this
+                    //    // console.log(888)
+                if ($self.attr('onoff') == 'true' ){
+                    //var key = $(this).parent().siblings('.title').find('span').text();
+                    //获取酷我id
+                    $.ajax({
+                        type: "get",
+                        dataType: "jsonp",
+                        jsonp: "callback",
+                        url: "https://api.lostg.com/music/" + song_id, //默认接口为虾米单曲
+                        data: {
+                            lyric: 1
+                        },
+                        async: !1,
+                        jsonCallback: 'searchMusicResult',
+                        success: function (data) {
+
+                            $('#warn').text('');
+                            //更改播放界面
+                            $('.cover').find('img').attr('src', data.album_pic);
+                            //<div class="info-name">好久不见</div>
+                            //<div class="info-album">《认了吧》</div>
+                            //<div class="info-singer">陈奕迅</div>
+                            $('.info-name').text(data.title)
+                            $(".info-album").text(data.album)
+                            $(".info-singer").text(data.singer)
+                            var url = data.location;
+                            $('.download a').attr('href',url);
+                            audio.src = url;
+                            startPlay($self);
+                            $self.attr('onoff',false);
+                            //歌曲时间处理
+                            var lyric = data.lyric;
+                            var lyric_1 = lyric.split('\n');
+                            //console.log(data)
+                            //console.log(audio.duration)
+                            function formate(t){
+                                t = parseInt(t)
+                                var min = parseInt(t / 60);
+                                    var sec = t % 60;
+                                    min = min < 10 ? '0' + min : min;
+                                    sec = sec < 10 ? '0' + sec : sec;
+                                    var str = min + ':' + sec
+                                    return str;
+                            }
+                            audio.ontimeupdate = function () {
+                            $('.count-time i').text(formate(audio.duration))
+                                    var str =formate(this.currentTime)
+                                    $('.count-time span').text(str);
+                                    //console.log(audio.src);
+                                    //更改进度条
+                                var per = parseInt((this.currentTime/audio.duration)*100)+'%';
+                                $('.live-process').css('width',per);
+                                }
+                                //console.log(audio.src)
+                                //console.log(audio.error)
+                          
+
+                        },
+                        error: function (status) {
+                            console.log('获取失败');
+                            $('#warn').text('没有资源');
+                            setTimeout(function(){
+                                $('#warn').text('');
+                            },1000)
+                        }
+                    });
+
+
+
+                } else {
+                    stopPlay($self);
+                   $self.attr('onoff',true);
+                }
+
+                // console.log(key)
+            })
+        }
+    })
+
+});
--- html/js/serch.js
+++ html/js/serch.js
@@ -61,38 +61,40 @@
                             dataType: 'jsonp',
                             jsonCallback: 'searchMusicResult',
                             success: function(data) {
+                                console.log(data)
                                 var path = data.abslist[0].WEBMP3Q0_PATH;
                                 var id = data.abslist[0].MUSICRID;
                                 console.log(id.match(/(\d+)/)[1])
                                     // console.log(id)
                                     $('audio')[0].pause();
                                 var url1 = 'http://other.web.ra01.sycdn.kuwo.cn/resource/' + path;
-                                var url2 = 'http://other.web.rb01.sycdn.kuwo.cn/resource/' + path;
-                                var url3 = 'http://other.web.rd01.sycdn.kuwo.cn/resource/' + path;
-                                var url4 = 'http://other.web.re01.sycdn.kuwo.cn/resource/' + path;
-                                var url5 = 'http://other.web.rh01.sycdn.kuwo.cn/resource/' + path;
-                                 var url6 = 'http://other.web.rf01.sycdn.kuwo.cn/resource/' + path;
-                                 var url7 = 'http://other.web.rc01.sycdn.kuwo.cn/resource/' + path;
+                                 var url2 = 'http://other.web.rb01.sycdn.kuwo.cn/resource/' + path;
+                                 var url3 = 'http://other.web.rd01.sycdn.kuwo.cn/resource/' + path;
+                                 var url4 = 'http://other.web.re01.sycdn.kuwo.cn/resource/' + path;
+                                 var url5 = 'http://other.web.rh01.sycdn.kuwo.cn/resource/' + path;
+                                  var url6 = 'http://other.web.rf01.sycdn.kuwo.cn/resource/' + path;
+                                  var url7 = 'http://other.web.rc01.sycdn.kuwo.cn/resource/' + path;
+                                 var url8 = 'http://other.web.rg01.sycdn.kuwo.cn/resource/' + path;
                                 console.log(url1)
 
-
-
-                                $('#audio1').attr("src", url1);
-                                $('#audio2').attr("src", url2);
-                                $('#audio3').attr("src", url3);
-                                $('#audio4').attr("src", url4);
-                                $('#audio5').attr("src", url5);
-                                $('#audio6').attr("src", url6);
-                                $('#audio7').attr("src", url7);
-                                $('#audio1')[0].play();
-                                $('#audio2')[0].play();
-                                $('#audio3')[0].play();
-                                $('#audio4')[0].play();
-                                $('#audio5')[0].play();
-                                $('#audio6')[0].play();
-                                $('#audio7')[0].play();
-
-                                self.css('backgroundImage', 'url(pause.png)')
+                                var i = 0;
+                                var url=[url1,url2,url3,url4,url5,url6,url7,url8]
+                                var audio = $('#audio')[0]
+                                audio.src= url[0];
+                                
+                                audio.play();
+                                audio.ontimeupdate=function(){
+                                    if(audio.error){
+                                        audio.src = url[(++i)%8];
+                                        audio.load();
+                                        audio.play();
+                                    }
+                                       
+                                    //console.log(audio.src);
+                                }
+                                //console.log(audio.src)
+                                //console.log(audio.error)
+                                self.css('backgroundImage', 'url(images/pause.png)')
                                 onOff = false;
 
 
@@ -107,14 +109,8 @@
 
 
                     } else {
-                        self.css('backgroundImage', 'url(play.png)')
-                        $('#audio1')[0].pause();
-                        $('#audio2')[0].pause();
-                        $('#audio3')[0].pause();
-                        $('#audio4')[0].pause();
-                        $('#audio5')[0].pause();
-                        $('#audio6')[0].pause();
-                        $('#audio7')[0].pause();
+                        self.css('backgroundImage', 'url(images/play.png)')
+                        audio.pause();
                         onOff = true;
                     }
 
