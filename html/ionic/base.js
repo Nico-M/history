a060384d3028c6b38ac64dda6d75f352597871d4
Nico-M <531521711@qq.com>
2016-12-10 19:27:51 +0800

    Update base.js

--- html/ionic/base.js
+++ html/ionic/base.js
@@ -50,7 +50,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
         })
     $urlRouterProvider.otherwise("/tab/sister");
 });
-app.controller('indexCtrl', ['$scope', '$http', function($scope, $http,$ionicLoading) {
+app.controller('indexCtrl', ['$scope', '$http','$ionicLoading', function($scope, $http,$ionicLoading) {
         $ionicLoading.show({
         template: '客官,我来啦',
     })
